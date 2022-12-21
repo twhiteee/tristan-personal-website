01eaff703989d65ff78c6bfe72db438f921b1e38
Tristan Whitehouse <tsw54@cornell.edu>
2022-12-21 13:42:00 -0500

    built career interests

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-/*
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-*/
+document.getElementById("shakySudsLink").onclick = function(){
+    document.getElementById("shakySudsLink").style.color = "purple";
+};
+document.getElementById("johnsonControlsLink").onclick = function(){
+    document.getElementById("johnsonControlsLink").style.color = "purple";
+};
+document.getElementById("amphenolSVMicrowaveLink").onclick = function(){
+    document.getElementById("amphenolSVMicrowaveLink").style.color = "purple";
+};
+document.getElementById("hogTechnologiesLink").onclick = function(){
+    document.getElementById("hogTechnologiesLink").style.color = "purple";
+};
+document.getElementById("airTechniquesLink").onclick = function(){
+    document.getElementById("airTechniquesLink").style.color = "purple";
+};
+document.getElementById("lufthansaTechnikLink").onclick = function(){
+    document.getElementById("lufthansaTechnikLink").style.color = "purple";
+};
